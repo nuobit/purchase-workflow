781ff91b8125744eba0999414f88715a2609eeaa
Eric Antones <eantones@nuobit.com>
2018-12-31 16:02:59 +0100

    [MIG] purchase_triple_discount: Migration to 11

--- purchase_triple_discount/__manifest__.py
+++ purchase_triple_discount/__manifest__.py
@@ -1,13 +1,13 @@
-# -*- coding: utf-8 -*-
+# Copyright 2018 NuoBiT - Eric Antones
 # Copyright 2017 Tecnativa - David Vidal
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': 'Account Invoice Triple Discount',
-    'version': '10.0.1.1.0',
+    'version': '11.0.1.0.0',
     'category': 'Purchase Management',
     'author': 'Tecnativa, '
               'Odoo Community Association (OCA)',
-    'website': 'https://tecnativa.com',
+    'website': 'https://odoo-community.org',
     'license': 'AGPL-3',
     'summary': 'Manage triple discount on purchase order lines',
     'depends': [
